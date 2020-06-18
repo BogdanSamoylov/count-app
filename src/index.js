da6871d378756ee5cef181fe39a0a03400ffe862
Bohdan <samoylov.bogdan@gmail.com>
2020-06-18 17:06:17 +0300

    Commit description

--- src/index.js
+++ src/index.js
@@ -1,38 +1,14 @@
+import React         from 'react';
+import ReactDOM      from 'react-dom';
 import {createStore} from 'redux';
-
-const reducer = (state = 0, action) => {
-  switch (action.type){
-    case 'btn-plus':
-      return state + 1;
-    case 'btn-minus':
-      return state - 1;
-    case 'btn-nullify':
-      return state*0;
-    default:
-      return state;
-  }
-}
-
-const plus = () => ({type:'btn-plus'});
-const minus = () => ({type:'btn-minus'});
-const nullify = () => ({type:'btn-nullify'});
+import {Provider}    from 'react-redux';
+import reducer       from './components/reducer';
+import App           from './components/app';
 
 const store = createStore(reducer);
 
-document.getElementById('btn-plus').addEventListener('click', () => {
-  store.dispatch(plus());
-});
-
-document.getElementById('btn-minus').addEventListener('click', () => {
-  store.dispatch(minus());
-});
-
-document.getElementById('btn-nullify').addEventListener('click', () => {
-  store.dispatch(nullify());
-});
-
-const update = () =>{
-  document.getElementById('count').textContent = store.getState();
-}
-
-store.subscribe(update);
+ReactDOM.render (
+  <Provider store = {store}>
+      <App/>
+  </Provider>
+  , document.getElementById('root'));
